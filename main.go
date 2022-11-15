eae2d3ae0dcfd18c1ac5f078112f20b5596c2162
Felix <felixxu06@gmail.com>
2022-11-15 23:13:12 +1300

    finis

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	i := 2
+	fmt.Print("Write ", i, " as ")
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("Today is weekend")
+	default:
+		fmt.Println("Today you need to work")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whoAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Println("Don't know type %T\n", t)
+		}
+	}
+
+	whoAmI(true)
+	whoAmI(23)
+	whoAmI("Are you OK")
 }
