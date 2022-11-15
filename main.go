5fd01623566fa46d89701f0e7bf52d19ab001632
Felix <felixxu06@gmail.com>
2022-11-15 21:32:05 +1300

    finish

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("go " + "demo values")
+
+	fmt.Println("1+1=", 1+1)
+
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+
+	fmt.Println("true && false is ", true && false)
+
+	fmt.Println("true || false is ", true || false)
+
+	fmt.Println("!true is ", !true)
 }
