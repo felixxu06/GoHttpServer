7d3ff89cb00c1aa18214d8debfc94ad1ad7e0773
Felix <felixxu06@gmail.com>
2022-11-15 21:40:28 +1300

    finish

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+const s string = "const s value"
+
+func main() {
+	fmt.Println("const s is ", s)
+
+	const n = 50000
+
+	const d = 3e20 / n
+	fmt.Println("const d is ", d)
+
+	fmt.Println("int64(d)", int64(d))
+
+	fmt.Println("math.Sin(n)", math.Sin(n))
 }
