2c0b8f1683f1c5dc540b0d4259826d03b008dd5b
Felix <felixxu06@gmail.com>
2022-11-15 21:36:19 +1300

    finish

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a = "initial"
+	fmt.Println("variable a is ", a)
+
+	var b, c int = 1, 2
+	fmt.Println("variable b c is ", b, c)
+
+	var d = true
+	fmt.Println("variable d is ", d)
+
+	var e int
+	fmt.Println("variable e is ", e)
+
+	f := "windows"
+	fmt.Println("variable f is ", f)
 }
