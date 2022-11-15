3781f458befa82cde522fee17746dfc5779a3cfd
Felix <felixxu06@gmail.com>
2022-11-15 21:25:19 +1300

    clean branch

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import "net/http"
-import "fmt"
-import "io"
+func main() {
 
-
-func main()  {
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/greeting", greetingHanlder)
-	http.ListenAndServe(":3333", nil)
-}
-
-func rootHandler(resp http.ResponseWriter, req *http.Request)  {
-	fmt.Printf("root handler invoked")
-	io.WriteString(resp, "Welcome to my website")
-}
-
-func greetingHanlder(resp http.ResponseWriter, req *http.Request)  {
-	fmt.Printf("greeting handler invoked")
-	io.WriteString(resp, "hello visitor, hoping you have a good day")
 }
