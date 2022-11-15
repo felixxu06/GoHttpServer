9659dc73c90b25e053be18c48c20016f69bc674e
Felix <felixxu06@gmail.com>
2022-11-15 21:50:27 +1300

    finish

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	i := 1
+	for i < 3 {
+		fmt.Println("i is ", i)
+		i++
+	}
+
+	i = 1
+	for {
+		fmt.Println("i is", i)
+
+		if i > 6 {
+			break
+		}
+		i++
+	}
+
+	for j := 0; j < 10; j++ {
+		fmt.Println("j is ", j)
+	}
 }
