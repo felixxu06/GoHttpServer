9ae6abd2c84a7f2aea0b16b227978c28e1e28f07
Felix Xu <felix.xu@elmosoftware.co.nz>
2022-11-16 17:03:05 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
+	m := make(map[string]int)
+
+	m["felix"] = 37
+	m["anita"] = 35
+
+	fmt.Println("map", m)
+	v1 := m["felix"]
+	fmt.Println("v1 is", v1)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "felix")
+	fmt.Println("map after delete", m)
+
+	_, prs := m["anita"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"xinxin": 7, "xincheng": 5}
+	fmt.Println("map n", n)
 }
