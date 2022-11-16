7a74da4b56fe28afa44a5737a85d381c29cd996d
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-17 11:04:15 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import "fmt"
+
+func vals() (int, int) {
+	return 3, 5
+}
+
 func main() {
+
+	a, b := vals()
+	fmt.Println("a is ", a, "b is ", b)
+
+	_, c := vals()
+	fmt.Println("c is ", c)
 }
