1db7d17ee6ec31f42d4be3e34f215b01ac984823
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-17 11:44:19 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
 func main() {
+
+	i := 10
+	fmt.Println("initial value of i is", i)
+
+	zeroval(i)
+	fmt.Println("after zeroval i is", i)
+
+	zeroptr(&i)
+	fmt.Println("after zeroptr i is", i)
 }
