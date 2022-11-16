eaa18b52a4d81121e99004b91a7d8539c682e48a
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-17 11:09:42 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import "fmt"
+
+func sum(nums ...int) {
+	fmt.Println(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+
+	fmt.Println(total)
+}
+
 func main() {
+	sum(1, 2)
+	sum(1, 2, 3, 4, 57, 88)
+
+	numbers := []int{1, 2, 3, 4, 5, 9, 6, 8}
+	sum(numbers...)
 }
