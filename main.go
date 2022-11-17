8aac76aeeaf8329ffb8fcb37d48dc5b7682f0054
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-17 21:15:42 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 func main() {
+	const s = "สวัสดี"
+
+	fmt.Println("Len:", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+
+	fmt.Println()
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for index, value := range s {
+		fmt.Printf("%#U starts at %d\n", value, index)
+	}
+
+	fmt.Println("\n Using DecodeRuneInstrings")
+
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Println("%#U starts at %d\n", runeValue, i)
+		w = width
+
+		examineRune(runeValue)
+	}
+}
+
+func examineRune(r rune) {
+	if r == 't' {
+		fmt.Println("found tee")
+	} else if r == 'a' {
+		fmt.Println("fount so sua")
+	}
 }
