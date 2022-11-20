b919244fca336c0e10804db3b0f9a2ac5abdb88f
Felix <felixxu06@gmail.com>
2022-11-20 20:02:17 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	myChannel := make(chan string, 2)
+
+	myChannel <- "first"
+	myChannel <- "second"
+
+	fmt.Println("the message from chan is ", <-myChannel)
+	fmt.Println("the message from chan is ", <-myChannel)
+
+	fmt.Println("end")
 }
