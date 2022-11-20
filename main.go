e2ebcfe4b0197f566310ef52233dd8fa01fefe6b
Felix <felixxu06@gmail.com>
2022-11-20 22:21:24 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import "fmt"
+
+func ping(msg string, channel chan string) {
+	fmt.Println("ping a message", msg)
+	channel <- msg
+}
+
+func pong(ping chan string, pong chan string) {
+	msg := <-ping
+	fmt.Println("get a message", msg)
+	pong <- msg
+}
+
 func main() {
+	pingC := make(chan string, 1)
+	pongC := make(chan string, 1)
+
+	ping("a message", pingC)
+	pong(pingC, pongC)
+
+	fmt.Println("end")
 }
