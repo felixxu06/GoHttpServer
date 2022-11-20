7aac5ff1dc1f6a582cb57923a94c8b8815ec9ee9
Felix <felixxu06@gmail.com>
2022-11-20 19:45:37 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
 func main() {
+	f("direct")
+
+	go f("goroutine")
+
+	f("direct2")
+
+	go func(msg string) {
+		fmt.Println("message:", msg)
+	}("going")
+	fmt.Println("after going")
+	time.Sleep(time.Second)
+
+	fmt.Println("end")
 }
