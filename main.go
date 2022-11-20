135a7109ff732e1dbeec643f2435aa926383104e
Felix <felixxu06@gmail.com>
2022-11-20 22:13:45 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func worker(signal chan bool) {
+	fmt.Println("worker is working")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	<-signal
+}
+
 func main() {
+	signal := make(chan bool, 1)
+	go worker(signal)
+	<-signal
 }
