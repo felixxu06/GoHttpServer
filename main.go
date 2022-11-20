019288026fcdfff7ee9c1c1e28bfc643de8fe21f
Felix <felixxu06@gmail.com>
2022-11-20 19:49:45 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	stringChannel := make(chan string)
+
+	go func() {
+		stringChannel <- "signal"
+	}()
+	fmt.Println("before get message")
+	msg := <-stringChannel
+	fmt.Println("after get message")
+
+	fmt.Println("message from channel", msg)
 }
