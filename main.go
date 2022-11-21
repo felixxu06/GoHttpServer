f2fa437c210b0b72363be544ee02f7b27c670351
Felix <felixxu06@gmail.com>
2022-11-21 23:42:01 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("go routine finish")
+				return
+			case t := <-ticker.C:
+				fmt.Println("tiker is ", t)
+			}
+		}
+	}()
+
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	done <- true
+
+	fmt.Println("ticker stopped")
 }
