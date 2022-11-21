98aca08c8aee5194c1bcdc50d0c6ce38d1f0d08f
Felix <felixxu06@gmail.com>
2022-11-22 00:43:48 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func woker(taskId int) {
+	fmt.Println("worker id is ", taskId, " started at", time.Now())
+	waitTime := rand.Int63n(5) + 1
+	time.Sleep(time.Second * time.Duration(waitTime))
+	fmt.Println("worker id is ", taskId, " end at", time.Now())
+}
+
 func main() {
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		j := i
+		go func() {
+			woker(j)
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+
 }
