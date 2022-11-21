08fc574ed4e2d084e697457573742b11ea906fef
Felix <felixxu06@gmail.com>
2022-11-21 23:06:58 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		jobs <- i
+		fmt.Println("send jobs", i)
+		time.Sleep(time.Second)
+	}
+
+	close(jobs)
+	fmt.Println("sent all jobs")
+	<-done
 }
