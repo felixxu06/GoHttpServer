901205e8806cc5c988b578ff9c63acfcf149d297
Felix <felixxu06@gmail.com>
2022-11-22 00:23:07 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func work(workerId int, jobs <-chan int, result chan<- string) {
+	for job := range jobs {
+		fmt.Println("work id:", workerId, " start work on job", job)
+		time.Sleep(time.Second)
+		fmt.Println("work id:", workerId, " finish work on job", job)
+
+		result <- fmt.Sprintln("work id:", workerId, " for job", job)
+	}
+}
+
 func main() {
+	jobNums := 5
+	jobs := make(chan int, jobNums)
+	result := make(chan string, jobNums)
+
+	for i := 0; i < 3; i++ {
+		go work(i, jobs, result)
+	}
+
+	for i := 0; i < jobNums; i++ {
+		jobs <- i * 5
+	}
+
+	close(jobs)
+
+	for i := 0; i < jobNums; i++ {
+		fmt.Println("result is :", <-result)
+	}
+
 }
