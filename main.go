71347dc6c81c774ed893ce0490baf2f9bb0387ca
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-27 20:01:54 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
 func main() {
+	var counter uint64
+
+	var waitgroup sync.WaitGroup
+
+	for i := 0; i < 50; i++ {
+		waitgroup.Add(1)
+
+		go func() {
+			for c := 0; c < 1000; c++ {
+				atomic.AddUint64(&counter, 1)
+			}
+			waitgroup.Done()
+		}()
+	}
+
+	waitgroup.Wait()
+
+	fmt.Println("the result is", counter)
 }
