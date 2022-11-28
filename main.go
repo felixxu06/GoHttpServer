6e6b384769520897787cfe4fce134c5822019164
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-29 11:58:13 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
+	strings := []string{"a", "c", "Anita", "Felix", "Nancy", "J&J"}
+
+	sort.Strings(strings)
+
+	fmt.Println("Strings:", strings)
+
+	ints := []int{8, 10, 1}
+
+	sort.Ints(ints)
+	fmt.Println(ints)
 }
