0a15204efc0725fc23ee1e8a0c8a1178f6ed7d9b
felix.xu <felix.xu@elmosoftware.co.nz>
2022-11-29 23:19:42 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func main() {
+	const expression = "p([a-z]+)ch"
+	match, _ := regexp.MatchString(expression, "peach")
+	fmt.Println("is match:", match)
+
+	r, _ := regexp.Compile(expression)
+
+	fmt.Println(r.MatchString("padadsch"))
+
+	fmt.Println(r.FindString("asdads, peasdfasdch"))
+
+	fmt.Println("idx:", r.FindStringIndex("aldfj, padfach"))
+
+	fmt.Println(r.FindStringSubmatch("paldfkach adflkalsldk "))
+
 }
