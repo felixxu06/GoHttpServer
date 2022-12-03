865e2f44edeebdb70b0aebacdb9ae7280d419429
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-04 11:13:31 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	now := time.Now()
+
+	fmt.Println(now)
+
+	fmt.Println("Unix time is ", now.Unix())
+	fmt.Println("Unix Milli is ", now.UnixMilli())
+	fmt.Println("Unix Nano is", now.UnixNano())
+
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
 }
