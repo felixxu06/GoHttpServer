fe546e21c1155647fdaae37329f152a3aa5c7922
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-04 11:09:48 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	p := fmt.Println
+
+	now := time.Now()
+
+	p(now)
+
+	then := time.Date(2019, 11, 11, 11, 50, 56, 111212, time.UTC)
+	p(then)
+
+	p(then.Year())
+
+	p(then.Weekday())
+
+	p(then.Before(now))
+
+	p(then.Equal(now))
+
+	diff := now.Sub(then)
+	p(diff)
+
+	p(diff.Hours())
 }
