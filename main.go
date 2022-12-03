908e24f7e2d882ea692f8449ee920bac5b6d91b7
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-04 11:54:26 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
+	f, _ := strconv.ParseFloat("1.2345", 64)
+	fmt.Println(f)
+
+	i, _ := strconv.ParseInt("1212", 0, 64)
+	fmt.Println(i)
+
+	d, _ := strconv.ParseInt("0x1c8", 0, 64)
+	fmt.Println(d)
+
+	p := fmt.Println
+
+	u, _ := strconv.ParseUint("55665", 0, 64)
+	p(u)
+
+	k, _ := strconv.Atoi("135")
+	p(k)
+
+	_, e := strconv.Atoi("wat")
+	p(e)
 }
