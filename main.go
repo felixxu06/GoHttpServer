86a5f4d8565566219aeab86b1b2e528e20f123bd
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-04 23:25:03 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import "embed"
+
+var fileString string
+var fileByte []byte
+var folder embed.FS
+
 func main() {
+	print(fileString)
+	print(string(fileByte))
+
+	content, _ := folder.ReadFile("folder/file1.hash")
+	print(string(content))
+
+	content2, _ := folder.ReadFile("folder/file2.hash")
+	print(string(content2))
 }
