d34e8113023146e25226b1b2f617e1de77a6f90f
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-05 22:37:32 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numberPtr := flag.Int("numb", 42, "an int")
+
+	forkPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "svard", "a string var")
+
+	flag.Parse()
+
+	p := fmt.Println
+
+	p("word:", *wordPtr)
+	p("number:", *numberPtr)
+	p("fork:", *forkPtr)
+	p("svar:", svar)
+	p("tail:", flag.Args())
 }
