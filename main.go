044a14fe3a85a1163410a61eaa9d85a9a653932f
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-05 22:55:02 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	os.Setenv("FOO", "foovariable")
+
+	fmt.Println("Foo environment variable is", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
+
+	fmt.Println("======================================================")
+
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		fmt.Println("environment ", pair[0], " value is", pair[1])
+	}
 }
