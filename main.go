2c4c7b6dbb4923772338856d7fa6c1c2fa830e85
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-05 23:00:29 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	resp, err := http.Get("https://gobyexample.com")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("response status", resp.StatusCode)
+
+	scanner := bufio.NewScanner(resp.Body)
+
+	for i := 0; scanner.Scan() && i < 10; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
