861cfd20243ce85039b034a5758e58286a81799a
felix.xu <felix.xu@elmosoftware.co.nz>
2022-12-05 23:08:09 +1300

    finish

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v", name, h)
+		}
+	}
+}
+
 func main() {
+	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8080", nil)
 }
